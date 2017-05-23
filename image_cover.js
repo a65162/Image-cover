9e327dd2098c37bd2401e645054d663afde9315b
阿洧 <a65162@gmail.com>
2017-05-23 19:37:12 +0800

    新增自訂image 高度

--- image_cover.js
+++ image_cover.js
@@ -9,22 +9,29 @@
     $.extend(this.background, settings);
   };
 
-  Image_cover.prototype.cover = function() {
+  Image_cover.prototype.cover = function(custom_Height) {
     var settings = this.background;
     var banner = $(this.id);
     var img = banner.find('img');
+    var Height = custom_Height;
 
-    $("<img/>").attr('src', img.attr('src')).load(function() {
-      var img_Height = this.height;
-      img.css('display', 'none');
-      banner.css({
-        'background-position': settings.position,
-        'background-repeat': settings.repeat,
-        'background-size:': settings.size,
-        'background-attachment': settings.attachment,
-        'background-image': 'url(' + img.attr('src') + ')',
-        'height': img_Height
-      });
+    img.css('display', 'none');
+    banner.css({
+      'background-position': settings.position,
+      'background-repeat': settings.repeat,
+      'background-size:': settings.size,
+      'background-attachment': settings.attachment,
+      'background-image': 'url(' + img.attr('src') + ')',
+      'display':'block',
     });
+
+    if(custom_Height) {
+      banner.css('height', custom_Height);
+    }else {
+      $("<img/>").attr('src', img.attr('src')).load(function() {
+        var img_Height = this.height;
+        banner.css('height', img_Height);
+      });
+    }
   };
 })(jQuery);
