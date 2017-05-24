38097194f173cf20fc9144e2927ca6f61fe18d00
阿洧 <a65162@gmail.com>
2017-05-24 11:14:16 +0800

    rewrite setHeight function for responsive

--- image_cover.js
+++ image_cover.js
@@ -9,29 +9,50 @@
     $.extend(this.background, settings);
   };
 
-  Image_cover.prototype.cover = function(custom_Height) {
+  Image_cover.prototype.cover = function() {
     var settings = this.background;
     var banner = $(this.id);
     var img = banner.find('img');
-    var Height = custom_Height;
 
-    img.css('display', 'none');
-    banner.css({
-      'background-position': settings.position,
-      'background-repeat': settings.repeat,
-      'background-size:': settings.size,
-      'background-attachment': settings.attachment,
-      'background-image': 'url(' + img.attr('src') + ')',
-      'display':'block',
-    });
-
-    if(custom_Height) {
-      banner.css('height', custom_Height);
-    }else {
-      $("<img/>").attr('src', img.attr('src')).load(function() {
-        var img_Height = this.height;
-        banner.css('height', img_Height);
+    $("<img/>").attr('src', img.attr('src')).load(function() {
+      var img_Height = this.height;
+      img.css('display', 'none');
+      banner.css({
+        'background-position': settings.position,
+        'background-repeat': settings.repeat,
+        'background-size:': settings.size,
+        'background-attachment': settings.attachment,
+        'background-image': 'url(' + img.attr('src') + ')',
+        'display': 'block',
+        'height': img_Height
       });
-    }
+    });
   };
+
+  Image_cover.prototype.setHeight = function(custom_Height, ratio ) {
+    var banner = $(this.id);
+    var img = banner.find('img');
+
+    this.responsive = function(a,b) {
+      if (a > $(window).height()) {
+        var Height = $(window).height() * b;
+        banner.css({
+          'height': Height
+        });
+      } else {
+        banner.css({
+          'height': a
+        });
+      }
+    };
+    var that = this;
+
+    $("<img/>").attr('src', img.attr('src')).load(function() {
+      that.responsive(custom_Height,ratio);
+      $(window).resize(function() {
+        that.responsive(custom_Height,ratio);
+      });
+    });
+  };
+
 })(jQuery);
